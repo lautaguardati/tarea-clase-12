4b032b18850ae3cb9b5f708c7539a7791e4d6467
lautaguardati <lautaguardati@gmail.com>
2024-07-19 14:54:13 -0300

    Muestra características de un pokemon
    Muestra de un pokemon específico características: nombre, habilidades, experiencia, altura, peso y tipo

--- pokedex/src/pokedex.js
+++ pokedex/src/pokedex.js
@@ -72,6 +72,67 @@ function limpiarPokemonSeleccionado() {
 
 
 function mostrarCaracteristicas(caracteristicas) {
+    mostrarNombre(caracteristicas.name);
+    mostrarHabilidades(caracteristicas.abilities);
+    mostrarExperiencia(caracteristicas.base_experience);
+    mostrarAltura(caracteristicas.height);
+    mostrarPeso(caracteristicas.weight);
+    mostrarTipos(caracteristicas.types);
+}
+
+function mostrarNombre(nombre) {
+    const $nombrePokemon = document.createElement("div");
+    $nombrePokemon.id = "nombre-pokemon";
+    $nombrePokemon.textContent = nombre + " ";
+    $pokemonSeleccionado.appendChild($nombrePokemon);
+    $nombrePokemon.appendChild(document.createElement('br'));
+}
+
+
+function mostrarHabilidades(habilidades) {
+    const $nodoHabilidades = document.createElement('p');
+    $nodoHabilidades.textContent = "Habilidades:";
+
+    for (i = 0; i < habilidades.length; i++) {
+        if (i === habilidades.length - 1) {
+            $nodoHabilidades.textContent = $nodoHabilidades.textContent + " " + habilidades[i].ability.name + ".";
+        } else {
+            $nodoHabilidades.textContent = $nodoHabilidades.textContent + " " + habilidades[i].ability.name + ",";
+        }
+    }
+    $pokemonSeleccionado.appendChild($nodoHabilidades);
+}
+
+function mostrarExperiencia(experiencia) {
+    const $nodoExperiencia = document.createElement('p');
+    $nodoExperiencia.textContent = `Experiencia: ${experiencia}`;
+    $pokemonSeleccionado.appendChild($nodoExperiencia);
+}
+
+function mostrarAltura(altura) {
+    const $nodoAltura = document.createElement('p');
+    $nodoAltura.textContent = `Altura: ${altura}`;
+    $pokemonSeleccionado.appendChild($nodoAltura);
+}
+
+function mostrarPeso(peso) {
+    const $nodoPeso = document.createElement('p');
+    $nodoPeso.textContent = `Peso: ${peso}`;
+    $pokemonSeleccionado.appendChild($nodoPeso);
+}
+
+function mostrarTipos(tipos) {
+    const $nodoTipos = document.createElement('p');
+    $nodoTipos.textContent = "Tipo: "
+
+    for (i = 0; i < tipos.length; i++) {
+        if (i === tipos.length - 1) {
+            $nodoTipos.textContent = $nodoTipos.textContent + " " + tipos[i].type.name + "."
+        } else {
+            $nodoTipos.textContent = $nodoTipos.textContent + " " + tipos[i].type.name + ","
+        }
+    }
+    $pokemonSeleccionado.appendChild($nodoTipos)
 }
 
 
